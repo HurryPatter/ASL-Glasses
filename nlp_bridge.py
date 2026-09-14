93e9bb9811db6251273513fa863f1068e4a6bb15
Omar <omarhosni224466@gmail.com>
2026-09-14 03:57:26 +0300

    first evaluation and data, group name injection

--- nlp_bridge.py
+++ nlp_bridge.py
@@ -1,6 +1,10 @@
 from symspellpy import SymSpell, Verbosity
 import pkg_resources
 
+# Team names -- always recognized, independent of academic/normal mode.
+TEAM_NAMES = ["omar", "hagar", "laila", "nourhan", "hassan"]
+
+
 class NLPBridge:
     def __init__(self, mode="normal"):
         self.sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
@@ -11,8 +15,16 @@ class NLPBridge:
         )
         self.sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 
+        self._inject_names()
+
         self.mode = mode
         self._inject_domain_vocab()
+
+    def _inject_names(self):
+        """Boost the team's names so they don't get segmented/misspelled
+        away. Unlike academic-mode vocab, this always applies."""
+        for name in TEAM_NAMES:
+            self.sym_spell.create_dictionary_entry(name, 10**9)
 
     def _inject_domain_vocab(self):
         """Boost domain-specific words based on current mode."""
@@ -20,7 +32,7 @@ class NLPBridge:
             engineering_words = [
                 "microcontroller", "mediapipe", "convolutional", "latency",
                 "debounce", "segmentation", "wearable", "inference",
-                "bandwidth", "accelerometer", "gyroscope", "Laila"
+                "bandwidth", "accelerometer", "gyroscope",
             ]
             for word in engineering_words:
                 self.sym_spell.create_dictionary_entry(word, 10**9)
